c116c12a4c877bffb4df23156482abb6769f70e1
natetheadequate <natewhite345@gmail.com>
2020-11-19 11:50:42 -0500

    Create authdadd.php

---
+++ authdadd.php
@@ -0,0 +1,8 @@
+<form action='add.php' method='post'>
+<?php echo '<input hidden name="password" value="'.$password.'" />' ?>
+<label>First Name: <input name="first_name" /></label>
+<label>Last Name: <input name="last_name" /></label>
+<label>Middle Name: <input name="middle_name" /></label>
+<button type="submit" />
+
+
