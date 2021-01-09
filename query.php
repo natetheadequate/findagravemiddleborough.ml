ce4fa88c7f8584be3a2e7f6455be890a59d946dc
natetheadequate <natewhite345@gmail.com>
2021-01-08 21:14:28 -0500

    final data and stripdata, homepage

---
+++ query.php
@@ -0,0 +1,9 @@
+<?php
+    include 'db.php';
+    $db=new mysqli("sd","asd","asd","asd");
+    foreach($_POST as $name->$value){
+        ${$name}=$value;
+    }
+    split($select)
+    $db->prepare('SELECT * from ')
+?>
--- stripdata.js
+++ stripdata.js
@@ -2,19 +2,34 @@ onload = () => {
     let tw = document.createTreeWalker(document.getElementsByTagName('table')[0], NodeFilter.SHOW_TEXT,
         { acceptNode: function (node) { if (/\S/.test(node.data)) { return NodeFilter.FILTER_ACCEPT } } })
     let el = tw.nextNode();
-    let i=0;
+    let i = 0;
     do {
-        const rawcolor=el.parentElement?.style?.color || getComputedStyle(el.parentElement).getPropertyValue('color');
-        let span=document.createElement("span");
-        span.style.color=rawcolor;
+        const rawcolor = el.parentElement?.style?.color || getComputedStyle(el.parentElement).getPropertyValue('color');
+        let span = document.createElement("span");
+        span.style.color = rawcolor;
         span.append(document.createTextNode(el.nodeValue));
-        let oldel=el;
-        if(!(el=tw.nextNode())){
+        let oldel = el;
+        if (!(el = tw.nextNode())) {
             break;
         }
         oldel.parentElement.appendChild(span);
         oldel.parentElement.removeChild(oldel);
-        console.log({rawcolor, span});
         i++;
     } while (true);
+    const data = document.getElementsByTagName('tbody')[0].innerHTML
+        .replaceAll(/\n/g, " ").replaceAll('|', '-')
+        .replaceAll(/<\/tr>\s*<tr[^>]*>/g, '@#@#@#@#')
+        .replaceAll(/<\/td>\s*<td[^>]*>/g, '|,|')
+        .replaceAll(/<\/?td[^>]*>/g, '|')
+        .replaceAll(/<\/?tr[^>]*>/g, '')
+        .replaceAll(/\|[^|]*<span style="color: rgb\(([^>]+)>\s*<\/span>[^|]*\|/g, '||') //clears out any blank good spans + their extraneous elements
+        .replaceAll(/\|[^|]*<span style="color: rgb\(([^<]*)<\/span>[^|]*\|/g, '|<span style="color: rgb($1</span>|') //clears out any extraneous elements of good spans with content
+        .replaceAll(/ +/g,' ')
+        .replaceAll(/\|\s*@#@#@#@#\s*\|/g,'|@#@#@#@#|');
+    const dataarr=data.split('@#@#@#@#');
+    const csvarr=[dataarr[0].replaceAll(/<[^>]*>/g,'')];
+    for(let i=1;i<dataarr.length;i++){
+        csvarr[i]=dataarr[i];
+    }
+    console.log(csvarr.join('\n'));
 }
