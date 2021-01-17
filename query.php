7495c61a8765255875cf1fa5ffd5601a2e5beaa6
natetheadequate <natewhite345@gmail.com>
2021-01-17 13:52:44 -0500

    progress

--- query.php
+++ query.php
@@ -1,31 +1,30 @@
 <?php
     include 'DB.php';
-    $DB=new mysqli();
     if(count($_POST['select'])<1){
         echo json_encode(["Nothing selected"]);
         exit;
     }
-    $columnsfile=$DB->query('DESCRIBE grave_data');
+    $columnsfile=$DB->query('DESCRIBE `grave_data`');
     $columns=[];
-    while($data=$columnsfile->fetch_array){
+    while($data=$columnsfile->fetch_array()){
         array_push($columns,$data["Field"]);
     }
     $selectvaluearr=[];
     foreach($_POST['select'] as $prospectiveselect){
         if(array_search($prospectiveselect,$columns)){
-            array_push($prospectiveselect,$selectvaluearr);
+            array_push($selectvaluearr,$prospectiveselect);
         }
     }
-    $selectstring=join(array_fill(0,count($selectarr),'?'),',');
+    $selectstring=join(',',array_fill(0,count($selectvaluearr),'?'));
     $wherearr=[];
     $wherevaluearr=[];
     $sortby='i';
     $sortorder="ASC";
     foreach($_POST as $key=>$value){
-        if(array_search($key,$columns)){
-            $v='`'.$key.'` LIKE "%?%"';
-            array_push($v,$wherearr);
-            array_push($value,$wherevaluearr);
+        if(array_search($key,$columns) && strlen($value)>0){
+            $v='`'.$key.'` LIKE ?';
+            array_push($wherearr,$v);
+            array_push($wherevaluearr,'%'.$value.'%');
         }elseif($key=='sortby' && array_search($value,$columns)){
             $sortby=$value;
         }elseif($key='sortorder'){
@@ -37,9 +36,10 @@
         }
     }
     $wherestring=implode(' AND ',$wherearr);
-    $querystring='SELECT '.$selectstring.' FROM `grave_data` WHERE '.$wherestring.' ORDER BY `'.$sortby.'` '.$sortorder; 
+    $querystring='SELECT '.$selectstring.' FROM `grave_data` WHERE '.$wherestring.' ORDER BY ? ?'; 
     $query=$DB->prepare($querystring);
-    $query->bind_param(str_repeat('s',count($selectvaluearr)+count($wherevaluearr)+2),implode(',',array_merge($selectvaluearr,$wherevaluearr,[$sortby],[$sortorder])));
+    $params=array_merge($selectvaluearr,$wherevaluearr,[$sortby],[$sortorder]);
+    $query->bind_param(str_repeat('s',count($selectvaluearr)+count($wherevaluearr)+2),...$params);
     $query->execute();
     echo json_encode(($query->get_result())->fetch_all());
 ?>
