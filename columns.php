3fbf0d729aad492c35e318cfba503a6fe066f642
natetheadequate <natewhite345@gmail.com>
2021-01-18 13:49:19 -0500

    columns.php no longer returns i

--- columns.php
+++ columns.php
@@ -2,7 +2,9 @@
 $columnsfile=$DB->query('DESCRIBE `grave_data`');
 $columns=[];
 while($data=$columnsfile->fetch_array()){
-    if(str_replace('notags','',$data)==$data){
-        array_push($columns,$data["Field"]);
+    if(str_replace('notags','',$data['Field'])==$data["Field"]){
+        if($data["Field"]!='i'){
+            array_push($columns,$data["Field"]);
+        }
     }
 }
