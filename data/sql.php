77d45095b6a339a9aafdeef1ef029a40f01476de
natetheadequate <natewhite345@gmail.com>
2021-01-23 13:24:04 -0500

    update not insert

--- data/sql.php
+++ data/sql.php
@@ -3,11 +3,11 @@
     $datafile=$DB->query('SELECT `Last_Name`,`Given_Name`,`Prefix`,`Suffix`,`i` from `grave_data`');
     $names=[];
     while($data=$datafile->fetch_array()){
-        array_push($name,[$data['i'],$data['Prefix'].' '.$data['Given_Name'].' '.$data['Last_Name'].' '.$data['Suffix']]);
+        array_push($names,[$data['i'],$data['Prefix'].' '.$data['Given_Name'].' '.$data['Last_Name'].' '.$data['Suffix']]);
     }
     $DB->query('ALTER TABLE `grave_data` ADD Name varchar(255)');
     foreach($names as $name){
-        $q=$DB->prepare('INSERT INTO `grave_data` (`Name`) VALUES ? WHERE i='.$name[0].';');
+        $q=$DB->prepare('UPDATE `grave_data` SET `Name`=? WHERE i='.$name[0].';');
         $q->bind_param('s',$name[1]);
         $q->execute();
     }
