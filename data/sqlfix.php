4c26ec9b364bcb2bf0bba6fa5e64d721b609dd90
natetheadequate <natewhite345@gmail.com>
2021-02-06 12:55:41 -0500

    path to DB

--- data/sqlfix.php
+++ data/sqlfix.php
@@ -1,5 +1,5 @@
 <?php 
-include 'DB.php';
+include '../DB.php';
 $datafile=$DB->query('Select Given_Name from `grave_data` where Given_Name Like "%oR%"');
 while($data=$datafile->fetch_array()){
     echo $data[0];
