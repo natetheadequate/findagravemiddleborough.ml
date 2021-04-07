e0fbe8826c748f417ed9c4c41c37c3a99e4c6e5b
natetheadequate <natewhite345@gmail.com>
2021-04-07 08:37:25 -0400

    new ideas

--- data/sqlfix.php
+++ data/sqlfix.php
@@ -1,6 +1,6 @@
 <?php 
 include '../DB.php';
-$datafile=$DB->query('Select Given_Name from `grave_data` where Given_Name Like "%oR%"');
+$datafile=$DB->query('Select Given_Name from `grave_data` where `Given_Name` Like "%oR%"');
 while($data=$datafile->fetch_array()){
     echo $data[0];
 }
