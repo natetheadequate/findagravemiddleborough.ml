0d4e705aa26575b2a3bca1f9c84683cfc3619340
natetheadequate <natewhite345@gmail.com>
2021-05-01 20:21:58 -0400

    fixes preg_match misunderstanding

--- data/dictionaries.php
+++ data/dictionaries.php
@@ -1,9 +1,7 @@
 <?php
 include '../DB.php';
-$DB=new mysqli();
-if(isset($_GET['dictionary'])){
-    $n=$_GET['dictionary'];
-    $datafile=$DB->query('select * from `'.preg_match('/^[a-z]+$/',$_GET['dictionary']).'`');
+if(isset($_GET['dictionary']) && preg_match('/[a-z_]+/',$_GET['dictionary'],$matches)){
+    $datafile=$DB->query('select * from `'.$matches[0].'`');
     $data=[];
     while($d=$datafile->fetch_assoc()){
         array_push($data,$d);
