e55761449a4a7387f912fd454b64bbf185e1e9eb
natetheadequate <natetheadequate@outlook.com>
2021-08-06 15:16:54 -0400

    moving getData() invocation to after html has rendered

--- public/api/edit.php
+++ public/api/edit.php
@@ -1,5 +1,7 @@
 <?php
-/*this is the page that admin is brought to when they click on a data point from the /edit page.
+/*this is the page that admin is brought to when they click on a data point from the /edit page, as well as the final page of the submit flow.
+
+
 It comes with GETS with the i and field of the data that was clicked on. 
 Also, when the form with the changes is submitted, it goes to an action page that then redirects back here with gets successful=true|timeout|password|false with a possible timeout get of the amount of seconds until another attempt should be tried (only if successful is timeout or password(because a failed password attempt locks password attempts for an increasing amount of time)) as well as an i and field */
 if (isset($_GET['id'])) {
@@ -42,7 +44,6 @@ if (isset($_GET['id'])) {
 				.then(res => res.text())
 				.then(data => displayData(data));
 		}
-		getData()
 	</script>
 </head>
 
@@ -67,11 +68,12 @@ if (isset($_GET['id'])) {
 		echo "Try again in ".$_GET['timeout']." seconds";
 	}?>
 	<form method="post" action="/editformactionpage.php">
-		Clear an input to be blank to have it be deleted
+		<p>Clear an input to be blank to have it be deleted</p>
 		<label>Password: <input type="password" required/></label>
 		<textarea name="field" hidden><?php echo $_GET['field'] ?></textarea>
 		<textarea name="id" hidden><?php echo $_GET['id'] ?></textarea>
 		<fieldset id="form"></fieldset>
 		<button type="submit">Submit</button>
 	</form>
+	<script>getData()</script>
 </body>
